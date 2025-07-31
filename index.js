e74495fb2a7457759acd9df7d742b3c3f013cb71
Abid-Hussain13 <nowaymate13$>
2025-07-31 20:49:14 +0500

    Fixing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
-mongoose.connect(process.env.MONGO_URL, {
+mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
