95d987657c7f6a9065f20f015871cb59f11f15a0
Abid-Hussain13 <nowaymate13$>
2025-07-31 18:34:25 +0500

    Inkspire Push

---
+++ index.js
@@ -0,0 +1,88 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+
+const app = express();
+const server = 3000;
+
+// Middleware
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs');
+
+mongoose.connect("mongodb://localhost:27017/blogApp")
+.then(() => console.log("MongoDB Connected"))
+.catch((err) => console.error("MongoDB connection error:", err));
+
+const blogSchema = new mongoose.Schema({
+    title: String,
+    content: String,
+    time: String,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+app.get('/', async(req, res) => {
+    const blogs = await Blog.find().sort({ _id: -1 });
+    res.render("index", { blogs});
+});
+
+app.get('/composser', (req, res) => {
+    res.render("composser");
+});
+
+app.get('/postview/:id',async (req, res) => {
+    const blogId = req.params.id; 
+    const blog = await Blog.findById(blogId);
+
+    if (blog) {
+        res.render("postview", { blog }); 
+    } else {
+        res.status(404).send("Blog not found"); 
+    }
+});
+app.get('/postdelete/:id', async (req, res) =>{
+    const blogId = req.params.id;
+    await Blog.findByIdAndDelete(blogId);
+
+    res.redirect('/');
+})
+app.get('/postedit/:id', async(req, res) =>{
+    const blogId = req.params.id;
+    const blog = await Blog.findById(blogId);
+
+    res.render("postEditor",{
+        blog : blog,
+    });
+})
+
+app.post('/submit', async(req, res) => {
+    const blogtitle = req.body.title;
+    const blogContent = req.body.content;
+    const blogTime = new Date().toLocaleString();
+    const newBlog = new Blog({ title: blogtitle, content: blogContent, time : blogTime }); 
+    await newBlog.save();
+    res.redirect("/"); 
+});
+app.post('/save/:id', async(req, res) => {
+    const blogId = req.params.id;
+    const blogTitle = req.body.title;
+    const blogContent = req.body.content;
+    try{
+        await Blog.findByIdAndUpdate(blogId, {
+            title: blogTitle,
+            content: blogContent,
+            time: new Date().toLocaleString(),
+        });
+        res.redirect('/');
+    }
+    catch(err){
+        res.status(500).send("Failed to update blog");
+    }
+    
+})
+
+// Server configuration
+app.listen(server, () => {
+    console.log(`Server is running on port ${server}`);
+});
